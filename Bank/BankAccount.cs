3a3785772907164feb2d2a1950b312966edbb355
Manar-Fuqha <manar0fuqha2002@gmail.com>
2024-11-25 19:25:28 +0300

    Implement Bank Class

--- Bank/BankAccount.cs
+++ Bank/BankAccount.cs
@@ -2,6 +2,42 @@
 {
     public class BankAccount
     {
+        private double balance;
+        public BankAccount() { }
 
+        public BankAccount(double balance)
+        {
+            this.balance = balance;
+        }
+
+        public double Balance()
+        {
+            return balance;
+        }
+
+        public void Add(double amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount));
+            }
+            balance += amount;
+        }
+
+        public void withDraw(double amount)
+        {
+            if(amount > balance) throw new ArgumentOutOfRangeException(nameof(amount));
+            if (amount < 0) throw new ArgumentOutOfRangeException(nameof(amount));
+            
+            balance -= amount;
+        }
+
+        public void TranserFundsTo( BankAccount otherAccount , double amount )
+        {
+            if(otherAccount is null )throw new ArgumentNullException(nameof(otherAccount));
+
+            withDraw(amount);
+            otherAccount.Add(amount);
+        }
     }
 }
